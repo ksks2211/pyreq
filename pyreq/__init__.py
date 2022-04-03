5516ff5d5cec35de60adb0219ab46679bfe99c89
ksks2211 <rival15@naver.com>
2022-04-03 17:58:58 +0900

    p

--- pyreq/__init__.py
+++ pyreq/__init__.py
@@ -1,3 +1,3 @@
 from .fetcher import fetch_res, fetch_text
 from .downloader import download
-from .html_collecter import collect_links
+from .collecter import collect_links
--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
     url="https://github.com/kkll22/pyreq",
     project_urls={"Bug Tracker": "https://github.com/kkll22/pyreq/issues"},
     license="MIT",
-    packages=["pyreq", "pyreq.fetcher", "pyreq.downloader", "pyreq.html_collector"],
+    packages=["pyreq", "pyreq.fetcher", "pyreq.downloader", "pyreq.collecter"],
     install_requires=["requests", "multimethod", "lxml", "cssselect"],
 )
